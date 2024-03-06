9f3bc3fac9d2acd8af69014587bd663127d3b1c9
DalpatRathore <69510006+DalpatRathore@users.noreply.github.com>
2024-03-06 22:32:34 +0530

    feat: data table to display courses

--- src/app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ src/app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -1,12 +1,27 @@
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import { DataTable } from "./_components/DataTable";
+import { columns } from "./_components/Columns";
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs";
 
-const Coursesage = () => {
+const Coursesage = async () => {
+  const { userId } = auth();
+
+  if (!userId) {
+    return;
+  }
+  const data = await db.course.findMany({
+    where: {
+      userId,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
   return (
     <div className="p-6">
-      <Link href={"/teacher/create"}>
-        <Button>Create</Button>
-      </Link>
+      <DataTable columns={columns} data={data} />
     </div>
   );
 };
