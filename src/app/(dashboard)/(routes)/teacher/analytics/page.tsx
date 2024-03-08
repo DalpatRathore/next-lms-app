2553928f9acd34f44908fb7185f5ab3b4c53508b
DalpatRathore <69510006+DalpatRathore@users.noreply.github.com>
2024-03-08 20:40:43 +0530

    feat: analytics with sales & revenue

--- src/app/(dashboard)/(routes)/teacher/analytics/page.tsx
+++ src/app/(dashboard)/(routes)/teacher/analytics/page.tsx
@@ -1,4 +1,27 @@
-const AnalyticsPage = () => {
-  return <div>AnalyticsPage</div>;
+import { getAnalytics } from "@/actions/getAnalytics";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import DataCard from "./_components/DataCard";
+
+const AnalyticsPage = async () => {
+  const { userId } = auth();
+
+  if (!userId) {
+    return redirect("/");
+  }
+
+  const { data, totalRevenue, totalSales } = await getAnalytics(userId);
+  return (
+    <div className="p-6">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mb-4">
+        <DataCard
+          label="Total Revenue"
+          value={totalRevenue}
+          shouldFormat
+        ></DataCard>
+        <DataCard label="Total Sales" value={totalSales}></DataCard>
+      </div>
+    </div>
+  );
 };
 export default AnalyticsPage;
