52fbe54f0c9e7f5d50032c9ccf61fa105466c446
DalpatRathore <69510006+DalpatRathore@users.noreply.github.com>
2024-03-08 11:03:16 +0530

    feat: integrated stipe pay sys

--- src/app/(course)/courses/[courseId]/chapters/[chapterId]/_components/CourseEnrollBtn.tsx
+++ src/app/(course)/courses/[courseId]/chapters/[chapterId]/_components/CourseEnrollBtn.tsx
@@ -2,6 +2,9 @@
 
 import { Button } from "@/components/ui/button";
 import { formatPrice } from "@/lib/format";
+import axios from "axios";
+import { useState } from "react";
+import toast from "react-hot-toast";
 
 interface CourseEnrollBtnProps {
   price: number;
@@ -9,8 +12,29 @@ interface CourseEnrollBtnProps {
 }
 
 const CourseEnrollBtn = ({ price, courseId }: CourseEnrollBtnProps) => {
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleClick = async () => {
+    try {
+      setIsLoading(true);
+
+      const response = await axios.post(`/api/courses/${courseId}/checkout`);
+
+      window.location.assign(response.data.url);
+    } catch (error) {
+      toast.error("Something went wrong.");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   return (
-    <Button className="w-full md:w-auto" size="sm">
+    <Button
+      className="w-full md:w-auto"
+      size="sm"
+      onClick={handleClick}
+      disabled={isLoading}
+    >
       Enroll for {formatPrice(price)}
     </Button>
   );
